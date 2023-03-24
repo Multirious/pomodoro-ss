0a2cfa78bbc1b180e3855fc9cc798454e9c771e9
Multirious <Multirious@outlook.com>
2023-03-24 13:55:07 +0700

    prog

--- src/main.rs
+++ src/main.rs
@@ -33,44 +33,90 @@ enum TrayInputEvent {
     RestartWork,
 }
 
-fn init_tray_item() -> Result<mpsc::Receiver<TrayInputEvent>> {
+fn init_tray_item() -> Result<(mpsc::Receiver<TrayInputEvent>, tray_item::TrayItem)> {
     let mut tray_item = os::tray_item()?;
-    let (tray_item_send, tray_item_recv) = mpsc::sync_channel(10);
+    let (tray_item_send, tray_item_recv) = mpsc::sync_channel(2);
     let tray_item_send_cloned = tray_item_send.clone();
     tray_item.add_menu_item("Quit", move || {
         let _result = tray_item_send_cloned.try_send(TrayInputEvent::Quit);
     })?;
-    Ok(tray_item_recv)
+    Ok((tray_item_recv, tray_item))
 }
 
 fn main() -> Result<()> {
-    let tray_item_recv = init_tray_item()?;
+    let (tray_item_recv, tray_item) = init_tray_item()?;
 
     let mut break_notifier = break_notifier::BasicTimeBreak::new(
         BreakState::NotBreak,
-        Duration::from_secs(10),
-        Duration::from_secs(10),
+        Duration::from_secs(5),
+        Duration::from_secs(5),
     );
-    break_notifier.set_start_break_callback(Some(|| {}));
-    break_notifier.set_end_break_callback(Some(|| {}));
+
+    let (break_send, break_recv) = mpsc::sync_channel(2);
+    let mut is_break = false;
+
+    let break_send_c = break_send.clone();
+    break_notifier.set_start_break_callback(Some(move || {
+        let res_ = break_send_c.send(true);
+    }));
+    break_notifier.set_end_break_callback(Some(move || {
+        let res_ = break_send.send(false);
+    }));
 
     main_loop_run(|world| {
-        match tray_item_recv.try_recv() {
-            Ok(o) => match o {
+        break_notifier.update(world);
+
+        if let Some(tray_input_event) = tray_item_recv.maybe_recv().break_res_err()? {
+            match tray_input_event {
                 TrayInputEvent::Quit => return ControlFlow::Break(Ok(())),
                 TrayInputEvent::RestartWork => todo!(),
-            },
-            Err(mpsc::TryRecvError::Empty) => {}
-            Err(e) => return ControlFlow::Break(Err(e)),
+            }
         }
-        break_notifier.update(world);
+        if let Some(recv_is_break) = break_recv.maybe_recv().break_res_err()? {
+            dbg!(recv_is_break);
+            is_break = recv_is_break;
+        }
+        let _res = os::block_input(is_break);
         ControlFlow::Continue(())
     })?;
 
     Ok(())
 }
 
-pub enum Void {}
+trait ResultIntoControLFlow<T, E> {
+    fn break_err(self) -> ControlFlow<E, T>;
+    fn break_res_err<TB>(self) -> ControlFlow<Result<TB, E>, T>;
+}
+
+impl<T, E> ResultIntoControLFlow<T, E> for Result<T, E> {
+    fn break_err(self) -> ControlFlow<E, T> {
+        match self {
+            Ok(o) => ControlFlow::Continue(o),
+            Err(e) => ControlFlow::Break(e),
+        }
+    }
+
+    fn break_res_err<TB>(self) -> ControlFlow<Result<TB, E>, T> {
+        match self {
+            Ok(o) => ControlFlow::Continue(o),
+            Err(e) => ControlFlow::Break(Err(e)),
+        }
+    }
+}
+
+trait MpscMaybeReceive<T> {
+    fn maybe_recv(&self) -> std::result::Result<Option<T>, mpsc::TryRecvError>;
+}
+
+impl<T> MpscMaybeReceive<T> for mpsc::Receiver<T> {
+    fn maybe_recv(&self) -> std::result::Result<Option<T>, mpsc::TryRecvError> {
+        match self.try_recv() {
+            Ok(o) => Ok(Some(o)),
+            Err(mpsc::TryRecvError::Empty) => Ok(None),
+            Err(e) => Err(e.into()),
+        }
+    }
+}
 
 pub fn main_loop_run<F, B>(mut f: F) -> B
 where
