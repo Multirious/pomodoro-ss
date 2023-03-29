fe3f3b85a8c186bd8c9d016fd98af6aa8d342b72
Multirious <Multirious@outlook.com>
2023-03-29 14:02:34 +0700

    m

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 use std::{
     ops::ControlFlow,
     sync::mpsc,
-    time::{Duration, Instant},
+    time::{Duration, Instant, SystemTime},
 };
 
 use anyhow::{bail, Error, Result};
@@ -19,7 +19,6 @@ use utils::*;
 mod activity_monitor;
 mod break_notifier;
 mod notification;
-mod os;
 mod schedule;
 mod time;
 mod tray_icon;
@@ -27,6 +26,7 @@ mod utils;
 
 pub struct World {
     delta: Duration,
+    system_since_start: &'static SystemTime,
 }
 
 impl World {
@@ -39,25 +39,50 @@ impl World {
 enum AppState {
     Break,
     NotBreak,
-    GettingUserBreakPreference,
+    Dialog,
+}
+
+impl AppState {
+    fn is_break(&self) -> bool {
+        if let AppState::Break = self {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+enum BreakPreference {
+    Hibernate { let_user_prepare: bool },
+    Shutdown { let_user_prepare: bool },
+    BlockInput { let_user_prepare: bool },
+}
+
+impl BreakPreference {
+    pub fn get() -> BreakPreference {
+        todo!();
+    }
 }
 
 fn main() -> Result<()> {
     let (tray_item_sender, tray_item_receiver) = mpsc::sync_channel(10);
     let tray_item = TrayItem::new_with_sender(TrayItemMode::default(), &tray_item_sender)?;
 
+    let break_duration = Duration::from_secs(5);
     let mut break_notifier = break_notifier::BasicTimeBreak::new(
         BreakState::NotBreak,
-        Duration::from_secs(5),
+        break_duration,
         Duration::from_secs(5),
     );
 
     let (break_send, break_recv) = mpsc::sync_channel(2);
-    let mut state = AppState::NotBreak;
+    let mut app_state = AppState::NotBreak;
 
     let break_send_c = break_send.clone();
     break_notifier.set_start_break_callback(Some(move || break_send_c.just_send(true)));
     break_notifier.set_end_break_callback(Some(move || break_send.just_send(false)));
+
+    let preparation_time = Duration::from_secs(30);
 
     main_loop_run(|world| {
         break_notifier.update(world);
@@ -67,7 +92,7 @@ fn main() -> Result<()> {
                 TrayInputEvent::Quit => return ControlFlow::Break(Ok(())),
                 TrayInputEvent::RestartWork => {
                     break_notifier.switch_to(BreakState::NotBreak);
-                    state = AppState::NotBreak;
+                    app_state = AppState::NotBreak;
                 }
                 TrayInputEvent::SkipWork { by } => {
                     if let BreakState::NotBreak = break_notifier.break_state() {
@@ -78,10 +103,17 @@ fn main() -> Result<()> {
         }
         if let Some(recv_is_break) = break_recv.maybe_recv().break_res_err()? {
             if recv_is_break {
-                state = AppState::GettingUserBreakPreference;
+                app_state = AppState::Dialog;
+                let break_preference = BreakPreference::get();
+                match break_preference {
+                    BreakPreference::Hibernate { let_user_prepare } => todo!(),
+                    BreakPreference::Shutdown { let_user_prepare } => todo!(),
+                    BreakPreference::BlockInput { let_user_prepare } => if let_user_prepare {},
+                }
+                app_state = AppState::Break;
             }
         }
-        let _res = block_input(state);
+        let _res = block_input(app_state.is_break());
         ControlFlow::Continue(())
     })?;
 
@@ -92,8 +124,12 @@ pub fn main_loop_run<F, B>(mut f: F) -> B
 where
     F: FnMut(&World) -> ControlFlow<B, ()>,
 {
+    static SINCE_START: once_cell::sync::Lazy<SystemTime> =
+        once_cell::sync::Lazy::new(SystemTime::now);
+
     let mut world = World {
         delta: Duration::ZERO,
+        system_since_start: &SINCE_START,
     };
     let mut last_frame_time = Instant::now();
     loop {
